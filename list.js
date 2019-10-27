796e8e52c7b04475a721c2889149d6f8a57ece87
kotobuki13 <kotobukigit@yahoo.co.jp>
2019-10-27 13:42:02 +0900

    単語リストページのデザインの完成

--- list.js
+++ list.js
@@ -1,21 +1,43 @@
 'use strict'
 {
 
-  const words = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f'
-  ];
+  const words = {
+    0: { word: "white", type: "名詞", meaning: "白", isDone: false },
+    1: { word: "marvelous", type: "形容詞", meaning: "素晴らしい", isDone: false },
+    2: { word: "car", type: "名詞", meaning: "車", isDone: false },
+    3: { word: "eat", type: "動詞", meaning: "食べる", isDone: false },
+    4: { word: "paper", type: "名詞", meaning: "紙", isDone: false },
+    5: { word: "dog", type: "名詞", meaning: "犬", isDone: false },
+    6: { word: "book", type: "動詞", meaning: "予約する", isDone: false },
+    7: { word: "light", type: "名詞", meaning: "光", isDone: false },
+    8: { word: "keyboard", type: "名詞", meaning: "キーボード", isDone: false },
+    9: { word: "street", type: "名詞", meaning: "道", isDone: false }
+  };
 
-  words.forEach((word, index) => {
+  Object.keys(words).forEach(function (key) {
+
+    const el = this[key];
+
     const li = document.createElement('li');
+    li.textContent = `${el.word}`;
+    document.querySelector('.WordName').appendChild(li);
+  }, words);
 
-    li.textContent = word;
+  Object.keys(words).forEach(function (key) {
 
-    document.querySelector('.list').appendChild(li);
-  });
+    const el = this[key];
 
+    const li = document.createElement('li');
+    li.textContent = `${el.type}`;
+    document.querySelector('.WordType').appendChild(li);
+  }, words);
+
+  Object.keys(words).forEach(function (key) {
+
+    const el = this[key];
+
+    const li = document.createElement('li');
+    li.textContent = `${el.meaning}`;
+    document.querySelector('.WordMeaning').appendChild(li);
+  }, words);
 }
--- main.js
+++ main.js
@@ -2,16 +2,16 @@
 
 {
   const words = {
-    0: { word: "white", class: "名詞", meaning: "白", isDone: false },
-    1: { word: "marvelous", class: "形容詞", meaning: "素晴らしい", isDone: false },
-    2: { word: "car", class: "名詞", meaning: "車", isDone: false },
-    3: { word: "eat", class: "動詞", meaning: "食べる", isDone: false },
-    4: { word: "paper", class: "名詞", meaning: "紙", isDone: false },
-    5: { word: "dog", class: "名詞", meaning: "犬", isDone: false },
-    6: { word: "book", class: "動詞", meaning: "予約する", isDone: false },
-    7: { word: "light", class: "名詞", meaning: "光", isDone: false },
-    8: { word: "keyboard", class: "名詞", meaning: "キーボード", isDone: false },
-    9: { word: "street", class: "名詞", meaning: "道", isDone: false }
+    0: { word: "white", type: "名詞", meaning: "白", isDone: false },
+    1: { word: "marvelous", type: "形容詞", meaning: "素晴らしい", isDone: false },
+    2: { word: "car", type: "名詞", meaning: "車", isDone: false },
+    3: { word: "eat", type: "動詞", meaning: "食べる", isDone: false },
+    4: { word: "paper", type: "名詞", meaning: "紙", isDone: false },
+    5: { word: "dog", type: "名詞", meaning: "犬", isDone: false },
+    6: { word: "book", type: "動詞", meaning: "予約する", isDone: false },
+    7: { word: "light", type: "名詞", meaning: "光", isDone: false },
+    8: { word: "keyboard", type: "名詞", meaning: "キーボード", isDone: false },
+    9: { word: "street", type: "名詞", meaning: "道", isDone: false }
   };
 
   let wordNow;
@@ -111,7 +111,7 @@
     wordNow = getWord();
 
     target.textContent = wordNow.word;
-    meaning.textContent = `【${wordNow.class}】 ${wordNow.meaning}`;
+    meaning.textContent = `【${wordNow.type}】 ${wordNow.meaning}`;
     startTime = Date.now();
     if (!noLimit) updateTimer();
   });
@@ -140,7 +140,7 @@
         }
       }
       updateTarget();
-      meaning.textContent = `【${wordNow.class}】 ${wordNow.meaning}`;
+      meaning.textContent = `【${wordNow.type}】 ${wordNow.meaning}`;
 
       score++;
       scoreLabel.textContent = score;
